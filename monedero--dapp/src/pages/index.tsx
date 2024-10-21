d607dd3838dfc7987ffe648a3f57fbf14c2bb809
jistro <contact@jistro.xyz>
2024-10-21 00:20:26 -0600

    fix bugs

--- monedero--dapp/src/pages/index.tsx
+++ monedero--dapp/src/pages/index.tsx
@@ -142,11 +142,7 @@ const Home: NextPage = () => {
         </Tabs.Root>
       </main>
 
-      <footer className={styles.footer}>
-        <a href="https://rainbow.me" rel="noopener noreferrer" target="_blank">
-          Made with ❤️ by your frens at 🌈
-        </a>
-      </footer>
+      
     </div>
   );
 };
